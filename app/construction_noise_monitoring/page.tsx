aa6a4f1fd7f14e65591e4d058ccbe1cde46736d7
Stavros Panagiotakopoulos <stavrospanagiotakopoulos024@gmail.com>
2023-10-23 07:41:52 -0700

    Update page.tsx
    Added Photo element

--- app/construction_noise_monitoring/page.tsx
+++ app/construction_noise_monitoring/page.tsx
@@ -3,6 +3,7 @@ import Features from '@/components/features';
 import FeaturesBlocks from '@/components/features-blocks';
 import Testimonials from '@/components/testimonials';
 import Newsletter from '@/components/newsletter';
+import Image from 'next/image';
 
 export default function ConstructingNoiseMonitoring() {
   return (
@@ -35,6 +36,33 @@ export default function ConstructingNoiseMonitoring() {
             <li>Construction Site Noise Surveys</li>
             <li>Noise Control Solutions</li>
           </ul>
+        </div>
+      </section>
+
+      {/* Team Photo Section */}
+      <section className="bg-gray-100 py-12">
+        <div className="max-w-6xl mx-auto px-4 sm:px-6 text-center">
+          <h2 className="text-3xl text-gray-800 mb-4">Meet Our Team</h2>
+          <div className="flex flex-wrap justify-center items-center">
+            <div className="p-4">
+              <Image
+                src="/team-member1.jpg" // Replace with your image path
+                alt="Team Member 1"
+                width={200}
+                height={200}
+              />
+              <p className="text-lg mt-2">John Doe</p>
+            </div>
+            <div className="p-4">
+              <Image
+                src="/team-member2.jpg" // Replace with your image path
+                alt="Team Member 2"
+                width={200}
+                height={200}
+              />
+              <p className="text-lg mt-2">Jane Doe</p>
+            </div>
+          </div>
         </div>
       </section>
 
