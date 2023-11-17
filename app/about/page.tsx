9988b808bfe3a933df4a1d14d917e3ca811ca12f
Stavros Panagiotakopoulos <stavrospanagiotakopoulos024@gmail.com>
2023-11-17 01:35:46 -0800

    changes about && construction background

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,11 +1,11 @@
-import Footer from '@/components/ui/footer';
-import Features from '@/components/features';
-import FeaturesBlocks from '@/components/features-blocks';
-import Testimonials from '@/components/testimonials';
-import Newsletter from '@/components/newsletter';
-import Image from 'next/image';
+
+
 
 export default function About() {
+
+    
+
+    
     return (
         <div>
             <section className="bg-gray-100">
@@ -34,31 +34,75 @@ export default function About() {
     <div className="container mx-auto py-16 px-4 sm:px-6 lg:px-8">
         <div className="grid grid-cols-1 md:grid-cols-2 items-center gap-8">
             <div className="max-w-lg">
-                
-                <p className="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
-                    eros at lacus feugiat hendrerit sed ut tortor. Suspendisse et magna quis elit efficitur consequat.
-                    Mauris eleifend velit a pretium iaculis. Donec sagittis velit et magna euismod, vel aliquet nulla
-                    malesuada. Nunc pharetra massa lectus, a fermentum arcu volutpat vel.</p>
-                <div className="mt-8">
-                    <a href="#" className="text-blue-500 hover:text-blue-600 font-medium">Learn more about us
-                        <span className="ml-2">&#8594;</span>
-                    </a>
+                <div className="mt-12 md:mt-0">
+                    <img src="https://images.unsplash.com/photo-1531973576160-7125cd663d86" alt="About Us Image" className="object-cover rounded-lg shadow-md" />
                 </div>
             </div>
-            <div className="mt-12 md:mt-0">
-            <div className="mt-12 md:mt-0">
-                            <img src="https://images.unsplash.com/photo-1531973576160-7125cd663d86" alt="About Us Image" className="object-cover rounded-lg shadow-md" />
-                        </div>
-                
+            <div className="max-w-lg">
+                <div className="mt-12 md:mt-0">
+                    <p className="mt-4 text-gray-600 text-lg">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed quis
+                        eros at lacus feugiat hendrerit sed ut tortor. Suspendisse et magna quis elit efficitur consequat.
+                        Mauris eleifend velit a pretium iaculis. Donec sagittis velit et magna euismod, vel aliquet nulla
+                        malesuada. Nunc pharetra massa lectus, a fermentum arcu volutpat vel.</p>
+                    <div className="mt-8">
+                        <a href="#" className="text-blue-500 hover:text-blue-600 font-medium">Learn more about us
+                            <span className="ml-2">&#8594;</span>
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
+    
+        
+
+
+
+
 </section>
+
+<section className="py-16 bg-gray-100 dark:bg-gray-800 font-poppins">
+  <div className="p-4 mx-auto max-w-7xl flex flex-col items-center">
+    <h2 className="pb-4 text-4xl font-bold text-center text-gray-800 md:text-6xl dark:text-gray-400">
+      Our Gallery
+    </h2>
+    <div className="mx-auto mb-8 border-b border-red-700 w-24 dark:border-gray-400"></div>
+    <div className="flex flex-wrap justify-center">
+      {Array.from({ length: 8 }, (_, index) => (
+        <div key={index} className="w-48 sm:w-36 md:w-48 mb-4 md:mb-0 px-2">
+          <a href="#">
+            <div className="relative mb-5 overflow-hidden rounded-md">
+              <img
+                className="object-cover w-full h-36 md:h-48 transition duration-500 group-hover:origin-center hover:scale-105 rounded-md"
+                src={`https://i.postimg.cc/Y23w2gc1/pexels-ricardo-esquivel-1586298.jpg`}
+                alt=""
+              />
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
+  </div>
+</section>
+
+
+
+
+
+
+
+
+
+
+
 
 
             
 
             
         </div>
+        
+        
+        
     );
 }
--- app/construction_noise_monitoring/page.tsx
+++ app/construction_noise_monitoring/page.tsx
@@ -6,52 +6,60 @@ import Newsletter from '@/components/newsletter';
 import Image from 'next/image';
 import Footer from '@/components/ui/footer';
 
+// Import your header background image
+const headerBackgroundImage = 'https://static.vecteezy.com/system/resources/thumbnails/002/082/615/small/header-with-black-and-blue-overlapped-arrows-abstract-modern-banner-with-place-for-your-text-material-design-abstract-widescreen-background-vector.jpg';
+
 export default function ConstructingNoiseMonitoring() {
   return (
     <>
-    <div>
-      <section className="bg-gradient-to-b from-gray-100 to-white">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6">
-          <div className="pt-32 pb-12 md:pt-40 md:pb-20">
-            {/* Page header */}
-            <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
-              <h1 className="h1">Welcome to Constructing Noise Monitoring</h1>
-              <p className="text-xl text-gray-600">Constructing noise monitoring is crucial for managing noise levels during construction. It ensures compliance with regulations and minimizes environmental impact. Advanced sound monitoring equipment accurately measures noise, preventing disturbances. Noise impact assessments identify risks, enabling proactive measures for mitigation. Real-time monitoring systems provide immediate data insights for timely adjustments. .</p>
+      <div>
+        <section
+          className="bg-black text-white"
+          style={{
+            backgroundImage: `url(${headerBackgroundImage})`,
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat',
+            minHeight: '50vh', // Adjust this height as needed
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+            
+          }}
+        >
+          <div className="max-w-6xl mx-auto px-4 sm:px-6">
+            <div className="pt-32 pb-12 md:pt-40 md:pb-20">
+              {/* Page header */}
+              <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
+                <h1 className="h1">Welcome to Constructing Noise Monitoring</h1>
+                <p className="text-xl text-gray-600">
+                  Constructing noise monitoring is crucial for managing noise levels during construction. It ensures compliance with regulations and minimizes environmental impact. Advanced sound monitoring equipment accurately measures noise, preventing disturbances. Noise impact assessments identify risks, enabling proactive measures for mitigation. Real-time monitoring systems provide immediate data insights for timely adjustments. .
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      
+        {/* Services Section */}
+        <section className="bg-white">
+          <div className="max-w-6xl mx-auto px-4 sm:px-6 py-16">
+            <h2 className="text-3xl text-center mb-8">Our Services</h2>
+            <p className="text-xl text-center text-gray-600">
+              We offer a range of comprehensive services for monitoring and controlling noise levels in construction environments. Our services include:
+            </p>
+            <ul className="list-disc list-inside text-lg mt-6">
+              <li>Noise Impact Assessments</li>
+              <li>Real-time Noise Monitoring</li>
+              <li>Construction Site Noise Surveys</li>
+              <li>Noise Control Solutions</li>
+            </ul>
+          </div>
+        </section>
 
-      {/* Services Section */}
-      <section className="bg-white">
-        <div className="max-w-6xl mx-auto px-4 sm:px-6 py-16">
-          <h2 className="text-3xl text-center mb-8">Our Services</h2>
-          <p className="text-xl text-center text-gray-600">
-            We offer a range of comprehensive services for monitoring and controlling noise levels in construction environments. Our services include:
-          </p>
-          <ul className="list-disc list-inside text-lg mt-6">
-            <li>Noise Impact Assessments</li>
-            <li>Real-time Noise Monitoring</li>
-            <li>Construction Site Noise Surveys</li>
-            <li>Noise Control Solutions</li>
-          </ul>
-        </div>
-      </section>
-
-      
-
-     
-
-      
-      
-    </div>
-
-    {/* Features Section */}
-    <Features />
-    <Footer />
-
+        {/* Features Section */}
+        <Features />
+        <Footer />
+      </div>
     </>
   );
 }
